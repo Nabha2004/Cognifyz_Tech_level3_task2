e56a8061c6791b70db85989f77dcfede3c510c27
Nabha2004 <nabhabagwan32@gmail.com>
2025-03-03 22:54:15 +0530

    logic developed

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const applyNowButton = document.getElementById('applyNowButton');
 
+    applyNowButton.addEventListener('click', () => {
+        const url = "https://forms.gle/CW4XomY4P89xxpAq7";
+        window.location.href = url;
+    });
+});
